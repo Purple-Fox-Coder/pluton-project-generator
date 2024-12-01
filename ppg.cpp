9fcdc3579450227f465ec8cad1a031734c3c5ee0
Purple-Fox-Coder <purplefoxcoder@gmail.com>
2024-12-01 15:04:12 -0600

    Add file creation logic

--- ppg.cpp
+++ ppg.cpp
@@ -3,10 +3,14 @@
 #include <fstream>
 #include <string>
 
+using std::filesystem::recursive_directory_iterator;
+using std::filesystem::is_regular_file;
 using std::filesystem::current_path;
 using std::filesystem::absolute;
 using std::filesystem::exists;
 using std::filesystem::path;
+using std::ofstream;
+using std::ifstream;
 using std::getline;
 using std::string;
 using std::cout;
@@ -16,6 +20,8 @@ using std::cin;
 static const string s_versionNumber = "0.1.0";
 
 void displayHelpMessage();
+void findAndReplace(const string &p_findString, const string &p_replaceString,
+  string &p_originalString);
 
 int main(int p_argCount, char const *p_argValues[]) {
 
@@ -105,16 +111,56 @@ int main(int p_argCount, char const *p_argValues[]) {
   }
 
   if (l_answer == "n" || l_answer == "N") {
-    cout << "Please re-enter command with corrections, for help use: ppg -h" << endl;
+    cout <<
+    "Please re-enter command with corrections, for help use: ppg -h" << endl;
     return 0;
   }
 
   // create project
+  for (const path &i_file : recursive_directory_iterator(l_templateDirectory)) {
+    if(is_regular_file(i_file)) {
+
+      string l_fileName =
+        string(i_file).substr(string(i_file).find_last_of("/\\") + 1);
+
+      findAndReplace("PROJECT_NAME", l_name, l_fileName);
+      findAndReplace("PROJECT_ALIAS", l_alias, l_fileName);
+
+      ifstream l_templateFile(i_file);
+      if(!l_templateFile.is_open()) {
+        cout << "Could not read " << i_file << endl;
+        continue;
+      }
+
+      ofstream l_outputFile(l_workingDirectory / l_fileName);
+      if (!l_outputFile.is_open()) {
+        cout << "Error creating the file " << l_fileName << endl;
+        continue;
+      }
+
+      string l_line = "";
+
+      while (getline(l_templateFile, l_line)) {
+        findAndReplace("$$PROJECT_NAME$$", l_name, l_line);
+        findAndReplace("$$PROJECT_ALIAS$$", l_alias, l_line);
+
+        l_outputFile << l_line << endl;
+      }
+
+      l_templateFile.close();
+      l_outputFile.close();
+
+      cout << "Successfully created " << l_fileName << endl;
+    } else {
+      cout << "Skipping " << i_file << " because it is a directory" << endl;
+    }
+  }
 
   return 0;
 }
 
 void displayHelpMessage() {
+
   cout <<
     "PPG version: " << s_versionNumber << endl <<
     endl <<
@@ -126,4 +172,20 @@ void displayHelpMessage() {
     "-a" << "  " << "Sets the project alias (-a <ALIAS>)" << endl <<
     "-h" << "  " << "Displays this message" << endl <<
     "-v" << "  " << "Displays the version number" << endl;
+}
+
+void findAndReplace(const string &p_findString, const string &p_replaceString,
+  string &p_originalString) {
+
+  size_t l_replacePosition = p_originalString.find(p_findString);
+
+  while (l_replacePosition != string::npos) {
+    p_originalString.replace(
+      l_replacePosition, p_findString.size(), p_replaceString
+    );
+
+    l_replacePosition = p_originalString.find(
+      p_findString, l_replacePosition + p_replaceString.size()
+    );
+  }
 }
